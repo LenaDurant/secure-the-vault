d193805cb50fbfee1150d510b82916f6cb15f6f4
LenaDurant <durant.arlena@gmail.com>
2024-06-28 15:42:56 -0400

    secure the vault

---
+++ index.js
@@ -0,0 +1,12 @@
+// add script tag to index file
+
+// tell user `You have received this message because you have been chosen to open an important vault. Here is the combination:`
+alert(`You have received this message because you have been chosen to open an important vault. Here is the secret combination:`);
+
+// Create three variables to equal the combination
+const vaultNumberOne = 1*6 + 14 + 14/2 - 17
+const vaultNumberTwo = 9/3 + 1 * 10 + 27
+const vaultNumberThree = 10*4-1 + 0
+
+// tell user the code
+alert(`The vault code is ${vaultNumberOne}-${vaultNumberTwo}-${vaultNumberThree}`);
